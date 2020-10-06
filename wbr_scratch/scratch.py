89cf9f88e92c9f6f945803cb8b93f9e2ae1cf645
Walter Reilly <wbreilly@ucdavis.edu>
2020-10-05 22:06:54 -0700

    deleted unnecessary stuff

--- wbr_scratch/scratch.py
+++ wbr_scratch/scratch.py
@@ -2,38 +2,59 @@
 # Created on Wed Sep 16 13:49:28 2020
 @author: WBR
 """
-# for key in tst:
-#      print(key)
-
-# for x,y in cna.graph.edges():
-#     print(x,y)
-    
-    
-# kinda works
-# note that it seems like a few nx methods don't work on CnaGraph class.
-# Maybe because the keys are objects themselves? idk but factory functions don't work
-# Have to manually pull out info as in graph_extractor.py 
-adj_mtx = nx.adjacency_matrix(cna.graph)
-plt.show(adj_mtx)
-
-[print(adj) for adj in g.adjacency()]
-
-# this shows me the stuff!
-[(n, nbrdict) for n, nbrdict in g.adj()]
+import os
+os.chdir('/Users/WBR/walter/diss_readerbenchpy/')
 
 
-# how to convert multi graph to adjacency matrix??
 import sys
 sys.path.append('/Users/WBR/walter/diss_readerbenchpy/readerbenchpy/wbr_scratch')
 
-from graph_extractor import compute_graph,create_df
+from graph_extractor import compute_graph,create_edge_df,create_node_df
 from rb.core.lang import Lang
+from pandas import DataFrame
 
 text = ['Mona was a good dog. She was the best dog named Mona.']
 models = [{"model":"word2vec","corpus":"coca"},{"model":"lsa","corpus":"coca"}]
 result = compute_graph(text,Lang.EN,models)
 
-df = create_df(result)
+df = create_edge_df(result)
+
+
+
+
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
